92a5aecb30f7529f55be61f3bea8af6077c6a36e
Camilo Rincon <camiloalex1643@gmail.com>
2019-08-04 21:52:13 -0500

    Estilos de la conversacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzinger';
+
+  //Injectar el router. Se coloca public para acceder desde el HTML
+  constructor (public router: Router){}
 }
